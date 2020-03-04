b767282bf2c6cbd2b7e2550bf1d87f647424be6a
ineggapps <inegg.apps@gmail.com>
2020-03-04 14:28:26 +0900

    스프링 프로젝트를 만들고 간단한 계산기 클래스를 구성하여 콘솔 창에 계산기로 계산한 결괏값이 출력되도록 하는 프로젝트 작성

---
+++ springapp/src/main/java/com/javalec/ex/Calculation.java
@@ -0,0 +1,55 @@
+package com.javalec.ex;
+
+public class Calculation {
+
+	private int firstNum;
+	private int secondNum;
+
+	public int getFirstNum() {
+		return firstNum;
+	}
+
+	public void setFirstNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+
+	public int getSecondNum() {
+		return secondNum;
+	}
+
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+
+	public void addition() {
+		System.out.println("더하기");
+		int result = firstNum + secondNum;
+		System.out.println(firstNum + " + " + secondNum + "=" + result);
+	}
+
+	public void subtraction() {
+		System.out.println("빼기");
+		int result = firstNum - secondNum;
+		System.out.println(firstNum + " - " + secondNum + " =  " + result);
+	}
+
+	public void add() {
+		System.out.println("add()");
+		System.out.println(firstNum + " + " + secondNum + " = " + (firstNum + secondNum));
+	}
+
+	public void sub() {
+		System.out.println("sub()");
+		System.out.println(firstNum + " - " + secondNum + " = " + (firstNum - secondNum));
+	}
+
+	public void mult() {
+		System.out.println("mult()");
+		System.out.println(firstNum + " * " + secondNum + " = " + (firstNum * secondNum));
+	}
+
+	public void div() {
+		System.out.println("div()");
+		System.out.println(firstNum + " / " + secondNum + " = " + (firstNum / secondNum));
+	}
+}
