6960db313fc5c3ed07dfdddc42520cd76d27b007
ineggapps <inegg.apps@gmail.com>
2020-03-05 15:28:02 +0900

    Container의 생명주기 Bean을 생성한 이후, 소멸 전에 특정 메서드 실행하는 예제 (어노테이션 또는 implements 인터페이스)

---
+++ spring_ex11_lifecycle/src/main/java/com/javalec/ex/Student.java
@@ -0,0 +1,41 @@
+package com.javalec.ex;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Student implements InitializingBean, DisposableBean {
+	private String name;
+	private int age;
+
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("afterPropertiesSet() 메서드 호출");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("destroy() 메서드 호출");
+	}
+
+}
