f734278b8ba022f057e9a29002d99e488a564e61
ineggapps <inegg.apps@gmail.com>
2020-03-26 22:33:49 +0900

    게시판 list목록 출력을 위한 메서드 작성

--- spring_lec16_spring_board/src/main/java/com/javalec/spring_pjt_board/dao/BDao.java
+++ spring_lec16_spring_board/src/main/java/com/javalec/spring_pjt_board/dao/BDao.java
@@ -1,5 +1,13 @@
 package com.javalec.spring_pjt_board.dao;
 
-public class BDao {
+import java.util.ArrayList;
 
+import com.javalec.spring_pjt_board.dto.BDto;
+
+public class BDao {
+	public ArrayList<BDto> list() {
+		ArrayList<BDto> dtos = null;
+		return dtos;
+	}
+	
 }
