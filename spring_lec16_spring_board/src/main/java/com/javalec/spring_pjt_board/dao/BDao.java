267e584c0d7e22314fca82a4934341d5fe86ff2f
ineggapps <inegg.apps@gmail.com>
2020-03-31 21:58:37 +0900

    JdbcTemplate를 이용하여 게시판 목록 불러오기.. (소름)

--- spring_lec16_spring_board/src/main/java/com/javalec/spring_pjt_board/dao/BDao.java
+++ spring_lec16_spring_board/src/main/java/com/javalec/spring_pjt_board/dao/BDao.java
@@ -11,13 +11,19 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.javalec.spring_pjt_board.controller.Constant;
 import com.javalec.spring_pjt_board.dto.BDto;
 
 public class BDao {
 
 	DataSource dataSource;
-
+	JdbcTemplate template = null;
+	
 	public BDao() {
+		//나중에 주석처리
 		try {
 			Context context = new InitialContext();
 			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/Oracle11g");
@@ -25,6 +31,8 @@ public class BDao {
 		} catch (NamingException e) {
 			e.printStackTrace();
 		}
+		
+		template = Constant.template;		
 	}
 
 	public void upHit(String bId) {
@@ -277,48 +285,54 @@ public class BDao {
 	}
 
 	public ArrayList<BDto> list() {
-		ArrayList<BDto> dtos = new ArrayList<BDto>();
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
-		try {
-			connection = dataSource.getConnection();
-			String query = "select bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent from mvc_board order by bGroup desc, bStep asc";
-			preparedStatement = connection.prepareStatement(query);
-			resultSet = preparedStatement.executeQuery();
-
-			while (resultSet.next()) {
-				int bId = resultSet.getInt("bId");
-				String bName = resultSet.getString("bName");
-				String bTitle = resultSet.getString("bTitle");
-				String bContent = resultSet.getString("bContent");
-				Timestamp bDate = resultSet.getTimestamp("bDate");
-				int bHit = resultSet.getInt("bHit");
-				int bGroup = resultSet.getInt("bGroup");
-				int bStep = resultSet.getInt("bStep");
-				int bIndent = resultSet.getInt("bIndent");
-
-				BDto dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
-				dtos.add(dto);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (resultSet != null) {
-					resultSet.close();
-				}
-				if (preparedStatement != null) {
-					preparedStatement.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+	
+		ArrayList<BDto> dtos = null;
+		String query = "select bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent from mvc_board order by bGroup desc, bStep asc";
+		dtos = (ArrayList<BDto>)template.query(query, new BeanPropertyRowMapper<BDto>(BDto.class));
 		return dtos;
+		
+//		ArrayList<BDto> dtos = new ArrayList<BDto>();
+//		Connection connection = null;
+//		PreparedStatement preparedStatement = null;
+//		ResultSet resultSet = null;
+//		try {
+//			connection = dataSource.getConnection();
+//			String query = "select bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent from mvc_board order by bGroup desc, bStep asc";
+//			preparedStatement = connection.prepareStatement(query);
+//			resultSet = preparedStatement.executeQuery();
+//
+//			while (resultSet.next()) {
+//				int bId = resultSet.getInt("bId");
+//				String bName = resultSet.getString("bName");
+//				String bTitle = resultSet.getString("bTitle");
+//				String bContent = resultSet.getString("bContent");
+//				Timestamp bDate = resultSet.getTimestamp("bDate");
+//				int bHit = resultSet.getInt("bHit");
+//				int bGroup = resultSet.getInt("bGroup");
+//				int bStep = resultSet.getInt("bStep");
+//				int bIndent = resultSet.getInt("bIndent");
+//
+//				BDto dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
+//				dtos.add(dto);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			try {
+//				if (resultSet != null) {
+//					resultSet.close();
+//				}
+//				if (preparedStatement != null) {
+//					preparedStatement.close();
+//				}
+//				if (connection != null) {
+//					connection.close();
+//				}
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		return dtos;
 	}
 
 }
