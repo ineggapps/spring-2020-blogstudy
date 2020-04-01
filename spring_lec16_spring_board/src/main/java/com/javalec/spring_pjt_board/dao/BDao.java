3099c1c23e02f48bccf470dd323d34eda69c60d4
ineggapps <inegg.apps@gmail.com>
2020-04-01 22:37:56 +0900

    JdbcTemplate 객체를 이용하여 CRUD작업 실시하기... (Amazing...)

--- spring_lec16_spring_board/src/main/java/com/javalec/spring_pjt_board/dao/BDao.java
+++ spring_lec16_spring_board/src/main/java/com/javalec/spring_pjt_board/dao/BDao.java
@@ -2,8 +2,7 @@ package com.javalec.spring_pjt_board.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Timestamp;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.naming.Context;
@@ -13,6 +12,8 @@ import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 
 import com.javalec.spring_pjt_board.controller.Constant;
 import com.javalec.spring_pjt_board.dto.BDto;
@@ -21,9 +22,9 @@ public class BDao {
 
 	DataSource dataSource;
 	JdbcTemplate template = null;
-	
+
 	public BDao() {
-		//나중에 주석처리
+		// 나중에 주석처리
 		try {
 			Context context = new InitialContext();
 			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/Oracle11g");
@@ -31,308 +32,107 @@ public class BDao {
 		} catch (NamingException e) {
 			e.printStackTrace();
 		}
-		
-		template = Constant.template;		
+
+		template = Constant.template;
 	}
 
-	public void upHit(String bId) {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		try {
-			connection = dataSource.getConnection();
-			String query = "update mvc_board set bHit = bHit+1 where bId = ?";
-			preparedStatement = connection.prepareStatement(query);
-			preparedStatement.setString(1, bId);
-			int rn = preparedStatement.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (preparedStatement != null) {
-					preparedStatement.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
+	public void upHit(final String bId) {
+		String query = "update mvc_board set bHit = bHit+1 where bId = ?";
+		template.update(query, new PreparedStatementSetter() {
+			@Override
+			public void setValues(PreparedStatement ps) throws SQLException {
+				ps.setInt(1, Integer.parseInt(bId));
 			}
-		}
+		});
 	}
 
-	public void replyShape(String strGroup, String strStep) {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		try {
-			connection = dataSource.getConnection();
-			String query = "update mvc_board set bStep = bStep + 1 where bGroup = ? and bStep > ?";
-			//bStep의 현재 값보다 1을 증가시킨다. 결론적으로 이전에 작성되었던 bStep의 수치가 하나씩 증가됨. 새로 작성된 답변글은 bStep이 수치가 하나씩 증가되기 이전의 수치(현잿값)부터 시작
-			System.out.println(String.format("update mvc_board set bStep = bStep + 1 where bGroup = %s and bStep > %s", strGroup,strStep));
-			preparedStatement = connection.prepareStatement(query);
-			preparedStatement.setInt(1, Integer.parseInt(strGroup));
-			preparedStatement.setInt(2, Integer.parseInt(strStep));
-			int rn = preparedStatement.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (preparedStatement != null)
-					preparedStatement.close();
-				if (connection != null)
-					connection.close();
-			} catch (Exception e) {
-				e.printStackTrace();
+	public void replyShape(final String strGroup, final String strStep) {
+		String query = "update mvc_board set bStep = bStep + 1 where bGroup = ? and bStep > ?";
+		template.update(query, new PreparedStatementSetter() {
+			@Override
+			public void setValues(PreparedStatement ps) throws SQLException {
+				ps.setInt(1, Integer.parseInt(strGroup));
+				ps.setInt(2, Integer.parseInt(strStep));
 			}
-		}
+		});
 	}
 
-	public void reply(String bId, String bName, String bTitle, String bContent, String bGroup, String bStep,
-			String bIndent) {
+	public void reply(final String bId, final String bName, final String bTitle, final String bContent,
+			final String bGroup, final String bStep, final String bIndent) {
 		replyShape(bGroup, bStep);
-		
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		try {
-			connection = dataSource.getConnection();
-			String query = "insert into mvc_board(bId, bName, bTitle, bContent, bGroup, bStep, bIndent) values(mvc_board_seq.nextval, ?, ?, ?, ?, ?, ?)";
-			preparedStatement = connection.prepareStatement(query);
-			preparedStatement.setString(1, bName);
-			preparedStatement.setString(2, bTitle);
-			preparedStatement.setString(3, bContent);
-			preparedStatement.setInt(4, Integer.parseInt(bGroup));
-			preparedStatement.setInt(5, Integer.parseInt(bStep)+1);
-			preparedStatement.setInt(6, Integer.parseInt(bIndent)+1);
-			int rn = preparedStatement.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (preparedStatement != null)
-					preparedStatement.close();
-				if (connection != null)
-					connection.close();
-			} catch (Exception e) {
-				e.printStackTrace();
+
+		String query = "insert into mvc_board(bId, bName, bTitle, bContent, bGroup, bStep, bIndent) values(mvc_board_seq.nextval, ?, ?, ?, ?, ?, ?)";
+		template.update(query, new PreparedStatementSetter() {
+			@Override
+			public void setValues(PreparedStatement ps) throws SQLException {
+				ps.setString(1, bName);
+				ps.setString(2, bTitle);
+				ps.setString(3, bContent);
+				ps.setInt(4, Integer.parseInt(bGroup));
+				ps.setInt(5, Integer.parseInt(bStep) + 1);
+				ps.setInt(6, Integer.parseInt(bIndent) + 1);
 			}
-		}
+		});
 	}
 
 	public BDto reply_view(String strbId) {
-		BDto dto = null;
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
-		try {
-			connection = dataSource.getConnection();
-			String query = "select * from mvc_board where bId=?";
-			preparedStatement = connection.prepareStatement(query);
-			preparedStatement.setInt(1, Integer.parseInt(strbId));
-			resultSet = preparedStatement.executeQuery();
-			if (resultSet.next()) {
-				int bId = resultSet.getInt("bId");
-				String bName = resultSet.getString("bName");
-				String bTitle = resultSet.getString("bTitle");
-				String bContent = resultSet.getString("bContent");
-				Timestamp bDate = resultSet.getTimestamp("bDate");
-				int bHit = resultSet.getInt("bHit");
-				int bGroup = resultSet.getInt("bGroup");
-				int bStep = resultSet.getInt("bStep");
-				int bIndent = resultSet.getInt("bIndent");
-				dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (resultSet != null) {
-					resultSet.close();
-				}
-				if (preparedStatement != null) {
-					preparedStatement.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return dto;
+		String query = "select * from mvc_board where bId= " + strbId;
+		return template.queryForObject(query, new BeanPropertyRowMapper<BDto>(BDto.class));
 	}
 
-	public void delete(String bId) {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		try {
-			connection = dataSource.getConnection();
-			String query = "delete from mvc_board where bid=?";
-			preparedStatement = connection.prepareStatement(query);
-			preparedStatement.setInt(1, Integer.parseInt(bId));
-			int rn = preparedStatement.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (preparedStatement != null)
-					preparedStatement.close();
-				if (connection != null)
-					connection.close();
-			} catch (Exception e) {
-				e.printStackTrace();
+	public void delete(final String bId) {
+		String query = "delete from mvc_board where bid=?";
+		template.update(query, new PreparedStatementSetter() {
+
+			@Override
+			public void setValues(PreparedStatement ps) throws SQLException {
+				ps.setString(1, bId);
 			}
-		}
+		});
 
 	}
 
-	public void modify(String bId, String bName, String bTitle, String bContent) {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		try {
-			connection = dataSource.getConnection();
-			String query = "update mvc_board set bName=?, bTitle=?, bContent=? where bId=?";
-			preparedStatement = connection.prepareStatement(query);
-			System.out.println("여기서 오류 발생했니?");
-			preparedStatement.setString(1, bName);
-			preparedStatement.setString(2, bTitle);
-			preparedStatement.setString(3, bContent);
-			preparedStatement.setInt(4, Integer.parseInt(bId));
-			int rn = preparedStatement.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (preparedStatement != null)
-					preparedStatement.close();
-				if (connection != null)
-					connection.close();
-			} catch (Exception e) {
-				e.printStackTrace();
+	public void modify(final String bId, final String bName, final String bTitle, final String bContent) {
+		String query = "update mvc_board set bName=?, bTitle=?, bContent=? where bId=?";
+		template.update(query, new PreparedStatementSetter() {
+			@Override
+			public void setValues(PreparedStatement ps) throws SQLException {
+				ps.setString(1, bName);
+				ps.setString(2, bTitle);
+				ps.setString(3, bContent);
+				ps.setInt(4, Integer.parseInt(bId));
 			}
-		}
+		});
 	}
 
 	public BDto contentView(String strbId) {
 
 		upHit(strbId);// 조회수 증가
+		String query = "select * from mvc_board where bId = " + strbId;
+		return template.queryForObject(query, new BeanPropertyRowMapper<BDto>(BDto.class));
 
-		BDto dto = null;
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
-
-		try {
-			System.out.println("조회 중..." + strbId);
-			connection = dataSource.getConnection();
-			String query = "select * from mvc_board where bId = ?";
-			preparedStatement = connection.prepareStatement(query);
-			preparedStatement.setInt(1, Integer.parseInt(strbId));
-			resultSet = preparedStatement.executeQuery();
-			if (resultSet.next()) {
-				int bId = resultSet.getInt("bId");
-				String bName = resultSet.getString("bName");
-				String bTitle = resultSet.getString("bTitle");
-				String bContent = resultSet.getString("bContent");
-				Timestamp bDate = resultSet.getTimestamp("bDate");
-				int bHit = resultSet.getInt("bHit");
-				int bGroup = resultSet.getInt("bGroup");
-				int bStep = resultSet.getInt("bStep");
-				int bIndent = resultSet.getInt("bIndent");
-
-				dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (resultSet != null)
-					resultSet.close();
-				if (preparedStatement != null)
-					preparedStatement.close();
-				if (connection != null)
-					connection.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		return dto;
 	}
 
-	public void write(String bName, String bTitle, String bContent) {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		try {
-			connection = dataSource.getConnection();
-			String query = "insert into mvc_board(bId, bName, bTitle, bContent, bHit, bGroup, bStep, bIndent) values(mvc_board_seq.nextval,?, ?, ?, 0, mvc_board_seq.currval, 0, 0)";
-			preparedStatement = connection.prepareStatement(query);
-			preparedStatement.setString(1, bName);
-			preparedStatement.setString(2, bTitle);
-			preparedStatement.setString(3, bContent);
-			int rn = preparedStatement.executeUpdate();
-			System.out.println("글쓰기 시도 => " + rn);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (preparedStatement != null)
-					preparedStatement.close();
-				if (connection != null)
-					connection.close();
-			} catch (Exception e) {
-				e.printStackTrace();
+	public void write(final String bName, final String bTitle, final String bContent) {
+		template.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				String query = "insert into mvc_board(bId, bName, bTitle, bContent, bHit, bGroup, bStep, bIndent) values(mvc_board_seq.nextval,?, ?, ?, 0, mvc_board_seq.currval, 0, 0)";
+				PreparedStatement pstmt = con.prepareStatement(query);
+				pstmt.setString(1, bName);
+				pstmt.setString(2, bTitle);
+				pstmt.setString(3, bContent);
+				return pstmt;
 			}
-		}
+		});
 	}
 
 	public ArrayList<BDto> list() {
-	
+
 		ArrayList<BDto> dtos = null;
 		String query = "select bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent from mvc_board order by bGroup desc, bStep asc";
-		dtos = (ArrayList<BDto>)template.query(query, new BeanPropertyRowMapper<BDto>(BDto.class));
+		dtos = (ArrayList<BDto>) template.query(query, new BeanPropertyRowMapper<BDto>(BDto.class));
 		return dtos;
-		
-//		ArrayList<BDto> dtos = new ArrayList<BDto>();
-//		Connection connection = null;
-//		PreparedStatement preparedStatement = null;
-//		ResultSet resultSet = null;
-//		try {
-//			connection = dataSource.getConnection();
-//			String query = "select bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent from mvc_board order by bGroup desc, bStep asc";
-//			preparedStatement = connection.prepareStatement(query);
-//			resultSet = preparedStatement.executeQuery();
-//
-//			while (resultSet.next()) {
-//				int bId = resultSet.getInt("bId");
-//				String bName = resultSet.getString("bName");
-//				String bTitle = resultSet.getString("bTitle");
-//				String bContent = resultSet.getString("bContent");
-//				Timestamp bDate = resultSet.getTimestamp("bDate");
-//				int bHit = resultSet.getInt("bHit");
-//				int bGroup = resultSet.getInt("bGroup");
-//				int bStep = resultSet.getInt("bStep");
-//				int bIndent = resultSet.getInt("bIndent");
-//
-//				BDto dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
-//				dtos.add(dto);
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		} finally {
-//			try {
-//				if (resultSet != null) {
-//					resultSet.close();
-//				}
-//				if (preparedStatement != null) {
-//					preparedStatement.close();
-//				}
-//				if (connection != null) {
-//					connection.close();
-//				}
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		return dtos;
 	}
 
 }
